14bcceadc764420f3829b3ac3eba0c671945f6dc
Vincent Nabet <1830490+vnabet@users.noreply.github.com>
2023-01-09 17:24:35 +0100

    bouton d'activation du mode daltonien

---
+++ script.js
@@ -0,0 +1,20 @@
+const link = document.querySelector('#daltonisme');
+
+console.log('LIEN', link);
+
+let daltonisme = !!parseInt(localStorage.getItem('daltonisme')??0);
+
+if(daltonisme) {
+  document.body.classList.add('daltonisme');
+}
+
+
+
+link && link.addEventListener('click', (e) => {
+
+  document.body.classList.toggle('daltonisme');
+
+  localStorage.setItem('daltonisme', document.body.classList.contains('daltonisme')?1:0);
+
+  e.preventDefault();
+})
